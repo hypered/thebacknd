c56a165d9ea4c57d6f09fdbb06e613d069e4f206
Vo Minh Thu <thu@hypered.io>
2024-04-13 14:27:17 +0200

    Move list_droplets() to the lib.

--- lib/thebacknd.py
+++ lib/thebacknd.py
@@ -11,3 +11,25 @@ do_client = pydo.Client(token=os.getenv("DIGITALOCEAN_ACCESS_TOKEN"))
 # Fingerprint or ID of an SSH key to embed in a created virtual machine. Must
 # already be known from DigitalOcean.
 ssh_key = os.getenv("EMBED_SSH_KEY")
+
+def list_droplets():
+    xs = do_client.droplets.list(tag_name="thebacknd")
+    r = {}
+    for x in xs["droplets"]:
+        # Normally, we already filter against this tag above,
+        # but better safe than sorry.
+        if "thebacknd" in x["tags"]:
+            r[x["id"]] = {
+                "name": x["name"],
+                "image-name": x["image"]["name"],
+                "image-description": x["image"]["description"],
+                "created_at": x["created_at"]
+            }
+            public_ips = [
+                network["ip_address"] for network in x["networks"]["v4"]
+                if network["type"] == "public"
+            ]
+            r[x["id"]]["public_ips"] = public_ips
+            if False:
+                r[x["id"]]["debug"] = x
+    return r
--- packages/thebacknd/list/__main__.py
+++ packages/thebacknd/list/__main__.py
@@ -1,27 +1,5 @@
 import thebacknd
 
-def list_droplets():
-    xs = thebacknd.do_client.droplets.list(tag_name="thebacknd")
-    r = {}
-    for x in xs["droplets"]:
-        # Normally, we already filter against this tag above,
-        # but better safe than sorry.
-        if "thebacknd" in x["tags"]:
-            r[x["id"]] = {
-                "name": x["name"],
-                "image-name": x["image"]["name"],
-                "image-description": x["image"]["description"],
-                "created_at": x["created_at"]
-            }
-            public_ips = [
-                network["ip_address"] for network in x["networks"]["v4"]
-                if network["type"] == "public"
-            ]
-            r[x["id"]]["public_ips"] = public_ips
-            if False:
-                r[x["id"]]["debug"] = x
-    return r
-
 def main(args):
-    xs = list_droplets()
+    xs = thebacknd.list_droplets()
     return xs
