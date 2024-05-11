77504c4319a13b2784491a3e7f83c753fca30453
Vo Minh Thu <thu@hypered.io>
2024-05-11 11:51:15 +0200

    The desired-system script matches update-system.
    The desired-system script was only watching a hard-coded URL instead of
    using the same logic than the update-system script. This is now fixed.

--- scripts/desired-system.sh
+++ scripts/desired-system.sh
@@ -2,4 +2,23 @@
 
 set -euo pipefail
 
-curl -s -o - https://f003.backblazeb2.com/file/hypered-store/toplevels/test.txt
+curl -s http://169.254.169.254/metadata/v1/user-data > user-data.json
+
+TOPLEVEL=$(jq -r '.nix_toplevel // empty' user-data.json)
+TOPLEVEL_URL=$(jq -r '.nix_toplevel_url // empty' user-data.json)
+
+if [ -n "$TOPLEVEL" ]; then
+  echo "Got a toplevel."
+
+elif [ -n "$TOPLEVEL_URL" ]; then
+  echo Querying URL for toplevel...
+  curl -s -o toplevel.txt "$TOPLEVEL_URL"
+  TOPLEVEL="$(cat toplevel.txt)"
+  rm toplevel.txt
+
+else
+  echo "No toplevel or toplevel URL provided."
+  exit 1
+fi
+
+echo ${TOPLEVEL}
